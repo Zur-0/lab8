34cc9be88772f0350c4b40bbfacd472c5e0b2b91
Егор Медянцев <Egor_Med@outlook.com>
2024-10-03 13:20:02 +0300

    Add new file timer.pas

---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿##
+var (hours,minutes,seconds):=Readinteger3('Введите кол-во часов, минут, секунд');
+assert((hours<=60) and (hours>=0) and (minutes<=60) and (minutes>=0) and (seconds<=60) and (seconds>=0));
+print('Суммарное количество секунд',(hours*3600+minutes*60+seconds));
+{Введите кол-во часов, минут, секунд 2 1 5
+Суммарное количество секунд 7265 }
